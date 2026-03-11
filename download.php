f30e3ed496f4715404f6d98b39fbe97c1c074ecd
gamaraf502 <gamaraf502@faxzu.com>
2026-03-10 19:39:47 -0700

    Add files via upload

---
+++ download.php
@@ -0,0 +1,415 @@
+{
+    "1": {
+        "comment": "",
+        "timestamp": 1759815671,
+        "type": "text"
+    },
+    "O1": {
+        "comment": "",
+        "timestamp": 1759815824,
+        "type": "text"
+    },
+    "primecode": {
+        "comment": "",
+        "timestamp": 1759902261,
+        "type": "text"
+    },
+    "fB": {
+        "comment": "",
+        "timestamp": 1760427233,
+        "type": "file",
+        "filename": "fB_Book1.xlsx"
+    },
+    "Share": {
+        "comment": "",
+        "timestamp": 1761229737,
+        "type": "text"
+    },
+    "QZ": {
+        "comment": "",
+        "timestamp": 1761229885,
+        "type": "text"
+    },
+    "12": {
+        "comment": "",
+        "timestamp": 1761229895,
+        "type": "text"
+    },
+    "D6": {
+        "comment": "",
+        "timestamp": 1762747676,
+        "type": "text"
+    },
+    "vps": {
+        "comment": "",
+        "timestamp": 1762848905,
+        "type": "text"
+    },
+    "java": {
+        "comment": "",
+        "timestamp": 1763007050,
+        "type": "text"
+    },
+    "dds": {
+        "comment": "",
+        "timestamp": 1763007108,
+        "type": "text"
+    },
+    "unit4": {
+        "comment": "",
+        "timestamp": 1763094033,
+        "type": "file",
+        "filename": "unit4_Unit 4 - Algebraic Structures & Morphisms_2025-26.pdf"
+    },
+    "MB": {
+        "comment": "",
+        "timestamp": 1763613141,
+        "type": "file",
+        "filename": "MB_join_query_2_cy.pdf"
+    },
+    "Lb": {
+        "comment": "",
+        "timestamp": 1763616953,
+        "type": "text"
+    },
+    "hu": {
+        "comment": "",
+        "timestamp": 1763616975,
+        "type": "text"
+    },
+    "IP": {
+        "comment": "",
+        "timestamp": 1763698108,
+        "type": "file",
+        "filename": "IP_Assignment 2-1.pdf"
+    },
+    "7i": {
+        "comment": "",
+        "timestamp": 1763698116,
+        "type": "file",
+        "filename": "7i_Assignment 2-1.pdf"
+    },
+    "zX": {
+        "comment": "",
+        "timestamp": 1763698124,
+        "type": "file",
+        "filename": "zX_Assignment 2-1.pdf"
+    },
+    "KP": {
+        "comment": "",
+        "timestamp": 1763698358,
+        "type": "file",
+        "filename": "KP_Assignment 1-1.pdf"
+    },
+    "DMQ3": {
+        "comment": "",
+        "timestamp": 1763700387,
+        "type": "text"
+    },
+    "1d": {
+        "comment": "",
+        "timestamp": 1763700739,
+        "type": "file",
+        "filename": "1d_Assignment 1-1.pdf"
+    },
+    "BM": {
+        "comment": "",
+        "timestamp": 1763700784,
+        "type": "file",
+        "filename": "BM_Assignment 1-1.pdf"
+    },
+    "HH": {
+        "comment": "",
+        "timestamp": 1763960509,
+        "type": "text"
+    },
+    "pF": {
+        "comment": "",
+        "timestamp": 1764163443,
+        "type": "file",
+        "filename": "pF_338676757.pdf"
+    },
+    "j6": {
+        "comment": "",
+        "timestamp": 1764165987,
+        "type": "file",
+        "filename": "j6_338680088.pdf"
+    },
+    "80": {
+        "comment": "",
+        "timestamp": 1764166003,
+        "type": "file",
+        "filename": "80_338680088.pdf"
+    },
+    "KU": {
+        "comment": "",
+        "timestamp": 1764166046,
+        "type": "file",
+        "filename": "KU_338680088.pdf"
+    },
+    "oopj": {
+        "comment": "",
+        "timestamp": 1764308799,
+        "type": "text"
+    },
+    "DE": {
+        "comment": "",
+        "timestamp": 1766544145,
+        "type": "text"
+    },
+    "de": {
+        "comment": "",
+        "timestamp": 1766544152,
+        "type": "text"
+    },
+    "s6": {
+        "comment": "",
+        "timestamp": 1768984058,
+        "type": "file",
+        "filename": "s6_CC P1.txt"
+    },
+    "4booklet": {
+        "comment": "",
+        "timestamp": 1769144057,
+        "type": "text"
+    },
+    "xr": {
+        "comment": "",
+        "timestamp": 1769228602,
+        "type": "text"
+    },
+    "PIET4OSLabManual": {
+        "comment": "",
+        "timestamp": 1769228641,
+        "type": "text"
+    },
+    "CISCO": {
+        "comment": "",
+        "timestamp": 1769566692,
+        "type": "text"
+    },
+    "cisco": {
+        "comment": "",
+        "timestamp": 1769566730,
+        "type": "text"
+    },
+    "sidel": {
+        "comment": "",
+        "timestamp": 1769571773,
+        "type": "text"
+    },
+    "leet": {
+        "comment": "",
+        "timestamp": 1769584642,
+        "type": "text"
+    },
+    "7": {
+        "comment": "",
+        "timestamp": 1769587060,
+        "type": "text"
+    },
+    "9": {
+        "comment": "",
+        "timestamp": 1769588032,
+        "type": "text"
+    },
+    "BOOM": {
+        "comment": "",
+        "timestamp": 1769749788,
+        "type": "file",
+        "filename": "BOOM_BOOM.HTML"
+    },
+    "Pass": {
+        "comment": "",
+        "timestamp": 1769750388,
+        "type": "text"
+    },
+    "Steam": {
+        "comment": "",
+        "timestamp": 1770001815,
+        "type": "text"
+    },
+    "m": {
+        "comment": "",
+        "timestamp": 1770173656,
+        "type": "text"
+    },
+    "snakegame": {
+        "comment": "",
+        "timestamp": 1770192427,
+        "type": "file",
+        "filename": "snakegame_game.py"
+    },
+    "496": {
+        "comment": "",
+        "timestamp": 1770192500,
+        "type": "text"
+    },
+    "c": {
+        "comment": "",
+        "timestamp": 1770192865,
+        "type": "text"
+    },
+    "k1": {
+        "comment": "",
+        "timestamp": 1770193631,
+        "type": "text"
+    },
+    "496-cccc": {
+        "comment": "",
+        "timestamp": 1770193811,
+        "type": "text"
+    },
+    "sc": {
+        "comment": "",
+        "timestamp": 1770193923,
+        "type": "text"
+    },
+    "st": {
+        "comment": "",
+        "timestamp": 1770353884,
+        "type": "file",
+        "filename": "st_START.bat"
+    },
+    "ss": {
+        "comment": "",
+        "timestamp": 1770354064,
+        "type": "file",
+        "filename": "ss_bat.txt"
+    },
+    "P1COMAparta": {
+        "comment": "",
+        "timestamp": 1770354458,
+        "type": "text"
+    },
+    "BAT": {
+        "comment": "",
+        "timestamp": 1770354989,
+        "type": "text"
+    },
+    "P1COMApartb": {
+        "comment": "",
+        "timestamp": 1770356154,
+        "type": "text"
+    },
+    "bat": {
+        "comment": "",
+        "timestamp": 1770356229,
+        "type": "file",
+        "filename": "bat_blackbox-output-code-RUNDNDGX5F.vbs"
+    },
+    "P2COMMA": {
+        "comment": "",
+        "timestamp": 1770957727,
+        "type": "text"
+    },
+    "main": {
+        "comment": "",
+        "timestamp": 1770959319,
+        "type": "text"
+    },
+    "GoogleAPI": {
+        "comment": "",
+        "timestamp": 1771928358,
+        "type": "text"
+    },
+    "D": {
+        "comment": "",
+        "timestamp": 1771985698,
+        "type": "text"
+    },
+    "P3": {
+        "comment": "",
+        "timestamp": 1772168982,
+        "type": "file",
+        "filename": "P3_p3B.docx"
+    },
+    "p3b": {
+        "comment": "",
+        "timestamp": 1772169600,
+        "type": "file",
+        "filename": "p3b_p3.pdf"
+    },
+    "AS": {
+        "comment": "",
+        "timestamp": 1772169745,
+        "type": "file",
+        "filename": "AS_P4.pdf"
+    },
+    "p4": {
+        "comment": "",
+        "timestamp": 1772169784,
+        "type": "file",
+        "filename": "p4_P4.pdf"
+    },
+    "sB": {
+        "comment": "pra4os",
+        "timestamp": 1772254035,
+        "type": "text"
+    },
+    "CD": {
+        "comment": "",
+        "timestamp": 1772255168,
+        "type": "file",
+        "filename": "CD_pp44 os.png"
+    },
+    "OSP4": {
+        "comment": "",
+        "timestamp": 1772255206,
+        "type": "file",
+        "filename": "OSP4_pp44 os.png"
+    },
+    "OSP5": {
+        "comment": "",
+        "timestamp": 1772255759,
+        "type": "text"
+    },
+    "OSLAB": {
+        "comment": "",
+        "timestamp": 1772256085,
+        "type": "file",
+        "filename": "OSLAB_OS Lab.pdf"
+    },
+    "Zm": {
+        "comment": "",
+        "timestamp": 1772536303,
+        "type": "text"
+    },
+    "httpsaistudioapps35495d77-247a-4": {
+        "comment": "",
+        "timestamp": 1772536362,
+        "type": "text"
+    },
+    "eO": {
+        "comment": "",
+        "timestamp": 1772734521,
+        "type": "text"
+    },
+    "nu": {
+        "comment": "",
+        "timestamp": 1773080537,
+        "type": "text"
+    },
+    "df": {
+        "comment": "",
+        "timestamp": 1773082650,
+        "type": "text"
+    },
+    "n4": {
+        "comment": "",
+        "timestamp": 1773083508,
+        "type": "file",
+        "filename": "n4_words_over_10.txt"
+    },
+    "DS": {
+        "comment": "",
+        "timestamp": 1773083620,
+        "type": "text"
+    },
+    "tL": {
+        "comment": "",
+        "timestamp": 1773084187,
+        "type": "file",
+        "filename": "tL_word_pool.py"
+    }
+}
---
+++ view.php
@@ -0,0 +1,14 @@
+Token             : No See https://thc.org/segfault/token
+Your workstation  : 152.59.4.44     (Vadodara/India)
+Reverse Port      : Type curl sf/port for reverse port.
+Exit cryptostorm  : 162.210.192.205 (Washington DC)
+Exit mullvad      : 194.114.136.126 (Osaka/Japan)
+TOR Proxy         : 172.20.0.111:9050
+Shared storage    : /everyone/RescueTable      (encrypted)
+Your storage      : /sec                       (encrypted)
+Your Onion WWW    : /onion                     (encrypted)
+Your Web Page     : http://ftk4x6klwl5uwkgk75smxlod5hvxy4ayjct42vicubivznxjx5fyilyd.onion/rescuetable/
+SSH               : ssh -o "SetEnv SECRET=xfMuNveOBMWqXtsjdWaJnrmz" root@lsd.segfault.net
+SSH (TOR)         : torsocks ssh -o "SetEnv SECRET=xfMuNveOBMWqXtsjdWaJnrmz" root@pwazc2ops4uitnwgmu6pkgqtyaoou5d3an4jvbfya3xrz2k63pfqawyd.onion
+SSH (gsocket)     : gsocket -s OGUyNjdhNmEM ssh -o "SetEnv SECRET=xfMuNveOBMWqXtsjdWaJnrmz" root@lsd.segfault.gsocket
+SECRET            : xfMuNveOBMWqXtsjdWaJnrmz <<<  WRITE THIS DOWN  <<<
